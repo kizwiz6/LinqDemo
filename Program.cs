c0f36a712b20b578e03b2f83ca62e2d3d230b371
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-13 17:13:03 +0000

    Challenge Complete: Print a list of only the B students (80% to 89%) in ascending order.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿internal class Program
+﻿using static System.Formats.Asn1.AsnWriter;
+
+internal class Program
 {
+    // make test scores
+    static int[] scores = { 50, 66, 90, 81, 77, 45, 0, 100, 99, 74, 85, 82, 80, 88, 89, 23, 43, 11, 15, 98 };
+
     private static void Main(string[] args)
     {
-        // make test scores
-        int[] scores = { 50, 66, 90, 81, 77, 45, 0, 100, 99, 74, 85, 82, 23, 43, 11, 15, 98 };
+
 
         // print the scores
         foreach (var item in scores)
@@ -41,6 +45,30 @@
             Console.WriteLine("One of the highest scores was {0}", item);
         }
 
+        Console.ReadLine();
+
+        bStudents();
+    }
+
+
+    public static void bStudents()
+    {
+        // use LINQ to filter the list
+        var theBStudents =
+            from item in scores
+            where item > 80 && item < 90
+            orderby item descending
+            select item;
+
+        // print only the best scores
+        foreach (var item in theBStudents)
+        {
+            Console.WriteLine("List of B student scores: {0}", item);
+        }
+
+        Console.WriteLine(theBStudents);
+
+        // Pause to see the output
         Console.ReadLine();
     }
 }
